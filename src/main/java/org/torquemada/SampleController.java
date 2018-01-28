0e1a02414c24a0602f495423ef9c1dfbd407f476
JolyRoger <mjelnr@gmail.com>
2018-01-28 14:17:39 +0100

    initial

---
+++ src/main/java/org/torquemada/SampleController.java
@@ -0,0 +1,27 @@
+package org.torquemada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    String home() {
+        return "home";
+    }
+
+    @RequestMapping("/hello")
+    String hello() {
+        return "hello";
+    }
+
+    @RequestMapping("/login")
+    String login() {
+        return "login";
+    }
+}
